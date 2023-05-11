83e5690b96c01d286bd7654c881897e283d15bd7
biplabroy1 <biplabroy7318@gmail.com>
2023-05-11 07:53:06 +0530

    REPL

---
+++ Youtube_Nodejs/tut3.js
@@ -0,0 +1,16 @@
+// ------------------------------------- REPL -----------------------------------
+/*
+The REPL feature of Node is very usefull to exeperiment with Node.js code and to debug javascript codes
+
+READ : Reads user input ,parses the input into javascript data Structure and store in memory
+
+EVAL : Takes and evalutes the data structure 
+
+Print : Prints the results 
+
+Loop : Loops the command untill the user presses ctrl-c twice
+
+In node environment 
+'_' give last result
+.editor : We can write the code 
+*/
